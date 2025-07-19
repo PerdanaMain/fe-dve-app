7a27ee7576e26b9fbca850c6b22b20526a6e12ed
perdanamain <firman.fp123@gmail.com>
2025-07-19 22:27:43 +0700

    chore: set up shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-const App = () => {
+import { Button } from "./components/ui/button";
+import { Loader2Icon } from "lucide-react";
+
+function App() {
   return (
-    <div>
-      <p className="text-amber-500">TESS</p>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button size="sm" >
+        <Loader2Icon className="animate-spin" />
+        Please wait
+      </Button>
     </div>
-  )
+  );
 }
 
-export default App  
+export default App;
